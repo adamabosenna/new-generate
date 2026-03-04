a49f16b0184709502255f2a23e8b90f2fe8d29cf
adamabosenna <adamabosenna10@gmail.com>
2026-03-04 13:56:09 +0200

    Updated clan mission logic in commands/clanmission.js as per user request.

--- commands/clanmission.js
+++ commands/clanmission.js
@@ -1,14 +1,38 @@
-// Updated clanmission.js to beautify responses
+// Updated clan mission command
 
-const operators = [
-    {name: 'Operator1', value: '1'},
-    {name: 'Operator2', value: '2'},
-    // ... other operators
-];
+const clanMission = async (missions) => {
+    // Step 1: Beautify operator names by removing numbers
+    const beautifiedMissions = missions.map(mission => {
+        return {
+            ...mission,
+            operatorNames: mission.operatorNames.map(name => name.replace(/\d+/g, '')) // Removing numbers
+        };
+    });
 
-function getOperators() {
-    return operators.map(operator => operator.name).join(', ');
-}
+    // Step 2: Logic to put operators with SAME stars in the LAST mission only 
+    const finalMissions = {};
 
-// Other existing functions
+    beautifiedMissions.forEach(mission => {
+        mission.operatorSlot.forEach(operator => {
+            const starRating = operator.stars;
+            if (!finalMissions[starRating]) {
+                finalMissions[starRating] = mission;
+            } else {
+                finalMissions[starRating].operatorSlot = finalMissions[starRating].operatorSlot.concat(operator);
+            }
+        });
+    });
 
+    // Create an array for the image generation with all 8 mission slots
+    const imageData = Object.values(finalMissions).slice(0, 8); // Assume we only need 8 missions
+
+    // Step 3: Pass correct data structure to image generator
+    passToImageGenerator(imageData);
+};
+
+const passToImageGenerator = (data) => {
+    // Logic to interface with image generator
+    console.log('Sending data to the image generator:', data);
+};
+
+module.exports = clanMission;
